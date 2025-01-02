19308303e072dce1fbf877fa59ce7ff592fefc1f
ParthPanchal26 <parthpanchal2625@gmail.com>
2025-01-02 19:22:07 +0530

    Corrected weather condition icon rendering

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,14 +4,16 @@ const Card = () => {
 
     const weather = useWeather();
 
+    const condition_icon = weather?.data?.current?.condition?.icon
+
     return (
         <div className="card">
-            <img src={weather?.data?.current?.condition?.icon || "src/assets/atmocel.svg"} style={{ color: 'white' }} alt="" />
+            <img src={condition_icon || "atmocel.svg"} style={{ color: 'white' }} alt="" />
             <h3>Sky Condition: <br /> <p>{weather?.data?.current?.condition?.text}</p></h3>
             <h2>Tamparature: {weather?.data?.current?.temp_c} C</h2>
             <h3>
-                City: <p>{weather?.data?.location?.name}</p> 
-                <br /> 
+                City: <p>{weather?.data?.location?.name}</p>
+                <br />
                 country: <p>{weather?.data?.location?.country}</p></h3>
         </div>
     )
